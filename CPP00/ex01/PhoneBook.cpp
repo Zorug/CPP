61cd0e03cd3318e25502c6030398669c7b4ae2c2
Cassiano Gross Schuler <cgross-s@c1r3s1.42porto.com>
2026-05-09 21:11:46 +0100

    update phonebook

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <iomanip>
 #include "PhoneBook.hpp"
 
-// primeiro construtor
-// "esta função pertence à classe PhoneBook"
 PhoneBook::PhoneBook()
 {
     currentIndex = 0;
     totalContacts = 0;
+}
+
+std::string formatField(std::string str)
+{
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";
+
+    return str;
 }
 
 void PhoneBook::addContact(
@@ -18,7 +25,6 @@ void PhoneBook::addContact(
     contacts[currentIndex].setLastName(lastName);
     contacts[currentIndex].setNickname(nickname);
 
-    // Se currentIndex = 7 -> Volta ao início.
     currentIndex = (currentIndex + 1) % 8;
 
     if (totalContacts < 8)
@@ -27,14 +33,22 @@ void PhoneBook::addContact(
 
 void PhoneBook::showContacts()
 {
+    std::cout << std::setw(10) << "Index" << "|";
+    std::cout << std::setw(10) << "First Name" << "|";
+    std::cout << std::setw(10) << "Last Name" << "|";
+    std::cout << std::setw(10) << "Nickname" << std::endl;
+
     for (int i = 0; i < totalContacts; i++)
     {
-        std::cout << i << " ";
+        std::cout << std::setw(10) << i << "|";
 
-        std::cout << contacts[i].getFirstName() << " ";
+        std::cout << std::setw(10)
+                  << formatField(contacts[i].getFirstName()) << "|";
 
-        std::cout << contacts[i].getLastName() << " ";
+        std::cout << std::setw(10)
+                  << formatField(contacts[i].getLastName()) << "|";
 
-        std::cout << contacts[i].getNickname() << std::endl;
+        std::cout << std::setw(10)
+                  << formatField(contacts[i].getNickname()) << std::endl;
     }
 }
