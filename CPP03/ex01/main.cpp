80a363108f47f200fb9fe4b516d40ff99e448bea
Cassiano Gross Schuler <cgross-s@c2r3s9.42porto.com>
2026-06-18 17:49:22 +0100

    main_update

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,16 +1,16 @@
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main()
 {
     ClapTrap robot1("R2D2");
-    ClapTrap robot2("C3PO");
+    robot1.attack("C3PO");
 
-    robot1.attack("C3PO");
-    robot2.takeDamage(0);
-    robot2.beRepaired(5);
-    robot1.attack("C3PO");
-    robot2.takeDamage(0);
-    robot2.beRepaired(3);
-    
+    std::cout << "---" << std::endl;
+
+    ScavTrap scav("Serena");
+    scav.attack("R2D2");
+    scav.guardGate();
+
     return 0;
 }
