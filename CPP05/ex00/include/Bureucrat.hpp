b1ff86b16d117f46ed0810b92479570770745847
Cassiano Gross Schuler <cgross-s@c2r2s5.42porto.com>
2026-07-10 19:40:45 +0100

    hpp

---
+++ CPP05/ex00/include/Bureucrat.hpp
@@ -0,0 +1,43 @@
+#ifndef BUREUCRAT_HPP
+#define BUREUCRAT_HPP
+
+class Bureucrat
+{
+private:
+	const	std::string _name;
+	int		_grade;
+public:
+	// Orthodox Canonical Form
+	Bureucrat();
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+
+	// Getters
+	const std::string& getName() const;
+	int getGrade() const;
+
+	// Methods
+	void incrementGrade();
+	void decrementGrade();
+
+	// exceptions
+	// std::exception is the base class to all exceptions in C++
+	class GradeTooHighException : public std::exception {
+	public:
+		// Method that returns the error message.
+		// throw() means that this function dont make exceptions
+		virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+};
+
+// Operator overload
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif
